8f160b2aff0b95f21fcba1f93b6f3b3c59bdb45d
Arushi Rai <arushirai1999@gmail.com>
2020-11-10 17:32:30 -0600

    Removed nans

--- models/simple_classifier_model.py
+++ models/simple_classifier_model.py
@@ -3,8 +3,9 @@ import keras
 class SimpleClassifier:
     def __init__(self, input_shape=600, num_classes=3, linear_layers=1):
         self.lin_layers = []
-        self.lin_layers.append(keras.layers.Dense(units=600,input_dim=input_shape, activation='relu', kernel_initializer='random_normal', bias_initializer='zeros'))
+        self.lin_layers.append(keras.layers.Dense(units=600,input_dim=input_shape, activation='relu', kernel_initializer='random_normal', bias_initializer='zeros')) #highest performing is this 600 neuron layer + output layer
         self.lin_layers.append(keras.layers.Dense(units=100, activation='relu', kernel_initializer='random_normal', bias_initializer='zeros'))
+        self.lin_layers.append(keras.layers.Dense(units=50, activation='relu', kernel_initializer='random_normal', bias_initializer='zeros'))
 
-        self.classifier = keras.layers.Dense(units=3, activation='softmax')
+        self.classifier = keras.layers.Dense(units=3, activation='softmax', kernel_initializer='random_normal', bias_initializer='zeros')
         self.model = keras.models.Sequential([*self.lin_layers, self.classifier])
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import numpy as np
 
 np.random.seed(0)
-params={'lr': 0.01, 'opt': 'adam', 'model': 'SimpleClassifier', 'loss': 'crossentropy', 'batch_size': 2000}
+params={'lr': 0.001, 'opt': 'adam', 'model': 'SimpleClassifier', 'loss': 'crossentropy', 'batch_size': 200}
 
 simple_classifier = SimpleClassifier()
 
@@ -17,12 +17,18 @@ lr_schedule = keras.optimizers.schedules.ExponentialDecay(
     decay_rate=0.9)
 opt = keras.optimizers.Adam(learning_rate=params['lr'])
 
-simple_classifier.model.compile(optimizer=opt, loss=keras.losses.CategoricalCrossentropy(), metrics=['accuracy', keras.metrics.Precision(), keras.metrics.Recall()])
+simple_classifier.model.compile(optimizer=opt, loss=keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])#, keras.metrics.Precision(), keras.metrics.Recall()])
+
+def filter_nans(arr):
+    arr=np.asarray(arr).astype(np.float64)
+    return np.isnan(np.sum(arr))
 
 # load data
 with open("train_df_w_embeddings.pickle", 'rb') as f:
     df = pickle.load(f)
     df['joint_embedding']=df['joint_embedding'].apply(lambda x: x.numpy()[0].tolist())
+    df['filter']=df['joint_embedding'].apply(lambda x: filter_nans(x))
+    df = df[df['filter']==False]
 
 joint_embedding_matrix = df['joint_embedding'].values
 
@@ -32,14 +38,22 @@ targets = label_encoder.fit_transform(df['label'].values)
 # convert targets into 1-hot form??
 # OneHotEncoder()
 joint_embedding_matrix=np.array(joint_embedding_matrix.tolist()).astype(np.float64)
+array_sum = np.sum(joint_embedding_matrix)
+array_sum = np.sum(array_sum)
+print(len([print(i, np.sum(item)) for i, item in enumerate(joint_embedding_matrix) if (np.isnan(np.sum(item)))]))
+array_has_nan = np.isnan(array_sum)
+print(array_has_nan)
 
 X_train, X_test, y_train, y_test = train_test_split(joint_embedding_matrix, targets, test_size=0.2)
 y_train = keras.utils.to_categorical(y_train, 3)
 y_test = keras.utils.to_categorical(y_test, 3)
+print(y_train.shape)
 
 simple_classifier.model.summary()
 simple_classifier.model.fit(X_train, y_train,
           batch_size=params['batch_size'],
           epochs=25,
           verbose=1, validation_data=(X_test, y_test))
+
 simple_classifier.model.save("./test/simple_classifier")
+
